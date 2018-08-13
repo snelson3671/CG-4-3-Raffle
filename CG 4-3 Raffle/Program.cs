ac92e7b8610d169af9fe7830465cf8a7f60ef7c4
Sandra <snelson3671@yahoo.com>
2018-08-13 14:58:11 -0500

    CG 4-3 Raffle

--- CG 4-3 Raffle/Program.cs
+++ CG 4-3 Raffle/Program.cs
@@ -6,7 +6,41 @@ namespace CG_4_3_Raffle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sandra's Raffle!");
+            Console.WriteLine("Enter up to 30 names of people who would like to enter the raffle.");
+            Console.WriteLine("Press enter after each name.  Then press enter twice when you are finished.");
+
+            int numberOfNames = 0;
+            string[] allNames = new string[30];
+
+            for (int i = 0; i <= 30; i++)
+            {
+                string name = Console.ReadLine();
+                if (name == "")
+                    break;
+
+                allNames[i] = name;
+                numberOfNames = i;
+            }
+
+            Random pickAName = new Random();
+            int raffleWinner = pickAName.Next(0, numberOfNames);
+            Console.WriteLine("The winner is " + allNames[raffleWinner]);
+
+            foreach (string name in allNames)
+            {
+                if (name == null)
+                    break;
+                    
+                if (allNames[raffleWinner] == name)
+                    continue;
+
+                Console.WriteLine(name + " you lost");
+            }
+
+            Console.ReadLine();
+
+            
         }
     }
 }
